170d12059012b45fa9b3c091452755c691656a7c
sohailshams <sohailshamas@yahoo.com>
2024-12-28 18:12:41 +0000

    Added react-router-dom and Test component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import "./App.css";
+import Test from "./components/Test";
 import { Button } from "./components/ui/button";
 import Login from "./components/ui/Login";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Button>ShadCN Button</Button>
       <p className="text-3xl font-bold underline text-red-500">Home Cook</p>
-      <Login />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
     </>
   );
 }
