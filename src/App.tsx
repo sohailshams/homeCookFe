4259a1a1d949ba87a52bb9a70e8d6aaedad6e967
sohailshams <sohailshamas@yahoo.com>
2025-01-01 23:03:26 +0000

    Updated route to render login page if user is unauthorized.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import "./App.css";
 import FoodList from "./components/FoodList";
 import Login from "./components/ui/Login";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { Toaster } from "sonner";
+import { useAuth } from "./contexts/AuthContext";
 
 function App() {
+  const { user } = useAuth();
   return (
     <>
       <Toaster position="bottom-left" />
-      <p className="text-3xl font-bold underline text-red-500">Home Cook</p>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/food-list" element={<FoodList />} />
+        <Route
+          path="/"
+          element={!user ? <Login /> : <Navigate to="/food-list" />}
+        />
+        <Route
+          path="/food-list"
+          element={user ? <FoodList /> : <Navigate to="/" />}
+        />
       </Routes>
     </>
   );
