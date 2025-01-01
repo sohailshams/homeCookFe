48aaa325491b57633d3828fe1e878a2d6e586c91
sohailshams <sohailshamas@yahoo.com>
2025-01-01 23:44:09 +0000

    Updated component to use react query to fetch food list from db.

--- src/components/FoodList.tsx
+++ src/components/FoodList.tsx
@@ -1,34 +1,56 @@
+import { useAuth } from "@/contexts/AuthContext";
 import axios from "axios";
 import { X } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { toast } from "sonner";
 
-const FoodList: React.FC = () => {
-  const [food, setFood] = useState();
-  console.log("food", food);
-
-  useEffect(() => {
-    const parameters = { useCookies: true };
-    axios
-      .get("https://localhost:7145/api/food", {
-        withCredentials: true,
-        params: parameters,
-      })
-      .then((response) => {
-        setFood(response.data);
-      })
-      .catch(() => {
-        toast.error("Error fetching food list from database.", {
-          duration: Infinity,
-          action: {
-            label: <X />,
-            onClick: () => toast.dismiss(),
-          },
-          id: "fetching-error-toast",
-        });
-      });
-  }, []);
-
-  return <h1>Food List</h1>;
+const fetchFoodList = async () => {
+  const parameters = { useCookies: true };
+  const response = await axios.get("https://localhost:7145/api/food", {
+    withCredentials: true,
+    params: parameters,
+  });
+  return response.data;
 };
+
+const FoodList: React.FC = () => {
+  const { user } = useAuth();
+
+  const {
+    data: food,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryFn: () => fetchFoodList(),
+    queryKey: ["foodList"],
+  });
+
+  console.log("food", food);
+  console.log("user", user);
+
+  if (isError) {
+    toast.error("Error fetching food list from database.", {
+      duration: Infinity,
+      action: {
+        label: <X />,
+        onClick: () => toast.dismiss(),
+      },
+      id: "fetching-error-toast",
+    });
+  }
+
+  if (isLoading) return <div>Loading food list...</div>;
+
+  return (
+    <div>
+      <h1>Food List</h1>
+      <ul>
+        {food?.map((item: { id: number; name: string }) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
 export default FoodList;
