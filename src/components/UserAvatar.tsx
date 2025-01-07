33a3709252570bd44aae1faad0016bf91c301002
sohailshams <sohailshamas@yahoo.com>
2025-01-07 23:12:38 +0000

    Created UserAvatar component to render user avatar.

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -1,6 +1,5 @@
 import { User } from "@/contexts/AuthContext";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import { CircleUserRound } from "lucide-react";
 
 type UserAvatarProps = {
   user: User | null;
@@ -13,8 +12,6 @@ function UserAvatar({ user }: UserAvatarProps) {
         <AvatarImage src={user?.userImageUrl} />
       ) : (
         <AvatarImage src="https://github.com/shadcn.png" />
-
-        // <CircleUserRound />
       )}
       <AvatarFallback>{user?.userEmail?.charAt(0)}</AvatarFallback>
     </Avatar>
