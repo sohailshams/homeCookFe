27a71ec3480590de26f81d6bcc861b00dd9af567
sohailshams <sohailshamas@yahoo.com>
2025-01-14 22:32:46 +0000

    Added Categories component in Layout.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import Categories from "./Categories";
 
 const Layout: React.FC = () => {
   return (
     <div>
       <Navbar />
+      <Categories />
       <main>
         <Outlet />
       </main>
