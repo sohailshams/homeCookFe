0342b0b98c36b7c085fa32c711fa54c559e94647
sohailshams <sohailshamas@yahoo.com>
2025-01-19 23:04:54 +0000

    Added Food type.

--- src/components/Types/Types.ts
+++ src/components/Types/Types.ts
@@ -7,3 +7,15 @@ export type Category = {
   id: number;
   name: string;
 };
+
+export type Food = {
+  id: number;
+  name: string;
+  foodImageUrls: string[];
+  price: number;
+  description: string;
+  category: Category;
+  quantityAvailable: number;
+  sellerId: number;
+  availableDate: string;
+};
