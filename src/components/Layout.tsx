327320a69d7d75cf106bdfe19af5acf2f22e2097
sohailshams <sohailshamas@yahoo.com>
2025-01-22 17:16:16 +0000

    Removed Categories from Layout component.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
-import Categories from "./Categories";
 
 const Layout: React.FC = () => {
   return (
     <div>
       <Navbar />
-      <Categories />
       <main>
         <Outlet />
       </main>
