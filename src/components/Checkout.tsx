f54a801f79c9676cdb5986321d8a95834cfb0ee1
sohailshams <sohailshamas@yahoo.com>
2025-02-02 23:46:06 +0000

    Implemented zod validations and added plus, minus button icons and related functions..

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -1,18 +1,78 @@
 import { useAuth } from "@/contexts/AuthContext";
 import { CircleMinus, CirclePlus } from "lucide-react";
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
+import * as zod from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+type LocationState = {
+  foodId: string;
+  quantity: number;
+  price: number;
+  name: string;
+  quantityAvailable: number;
+};
 
 const Checkout: React.FC = () => {
   const location = useLocation();
-  const { foodId, quantity, price, name } = location.state || {};
+  const { foodId, quantity, price, name, quantityAvailable } =
+    (location.state as LocationState) || {};
+  //   const [foodQuantity, setFoodQuantity] = useState<number>(Number(quantity));
   const { user } = useAuth();
+
+  const schema = zod.object({
+    foodQuantity: zod.coerce
+      .number({
+        required_error: "Quantity is missing.",
+      })
+      .min(1, { message: "Quantity must be at least 1." })
+      .max(quantityAvailable ?? 0, {
+        message: `Quantity must be less than or equal to ${
+          quantityAvailable ?? 0
+        }.`,
+      })
+      .int({
+        message: "Please add an integer.",
+      }),
+  });
+
+  type formFields = zod.infer<typeof schema>;
+
+  const resolver = zodResolver(schema);
+
+  const methods = useForm<formFields>({
+    defaultValues: { foodQuantity: quantity },
+    resolver,
+    mode: "onChange",
+  });
+
+  const {
+    register,
+    watch,
+    setValue,
+    trigger,
+    formState: { errors, isValid },
+  } = methods;
+
+  const increment = () => {
+    const currentQuantity = Number(watch("foodQuantity"));
+    setValue("foodQuantity", currentQuantity + 1);
+    trigger("foodQuantity");
+  };
+
+  const decrement = () => {
+    const currentQuantity = Number(watch("foodQuantity"));
+    setValue("foodQuantity", currentQuantity - 1);
+    trigger("foodQuantity");
+  };
 
   return (
     <div className="flex my-6 max-w-[90%] mx-auto flex-col lg:flex-row">
       <div className="w-full lg:w-3/5 grow">
         <form>
           <div className="flex flex-col space-y-4 p-4">
-            <h1 className="text-2xl font-bold">Checkout</h1>
+            <h1 className="text-2xl font-bold text-gray-700">Checkout</h1>
             <div className="flex flex-col space-y-2">
               <label htmlFor="first-name" className="hidden">
                 First Name
@@ -115,25 +175,32 @@ const Checkout: React.FC = () => {
           </div>
         </form>
       </div>
-      <div className="w-full lg:w-2/5 flex-none">
+      <div className="w-full lg:w-2/5 flex-none text-gray-700">
         <div className="p-4">
           <h1 className="text-2xl font-bold">Order Summary</h1>
           <div className="flex justify-between my-2">
             <p>{name}</p>
             <div className="flex items-center space-x-2">
-              <CircleMinus />
+              <CircleMinus className="cursor-pointer" onClick={decrement} />
               <input
-                className="w-10 outline-none border-[1px] border-gray-300 [&::-webkit-inner-spin-button]:appearance-none text-center"
+                className={`w-10 outline-none border-[1px] border-gray-300 [&::-webkit-inner-spin-button]:appearance-none text-center  ${
+                  errors.foodQuantity ? "border-red-500" : "border-gray-300"
+                }`}
                 type="number"
-                value={quantity}
+                {...register("foodQuantity")}
               />
-              <CirclePlus />
+              <CirclePlus className="cursor-pointer" onClick={increment} />
             </div>
             <p>£{price}</p>
           </div>
+          {errors.foodQuantity && (
+            <p className="text-red-500 text-sm mt-1 text-center">
+              {errors.foodQuantity.message}
+            </p>
+          )}
           <div className="flex justify-between my-2">
             <p>Total</p>
-            <p>£{price * quantity}</p>
+            <p>£{price * watch("foodQuantity")}</p>
           </div>
         </div>
       </div>
