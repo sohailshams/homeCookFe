8ce4f64014d7e5fedd0241946940b4d6f2d30b56
sohailshams <sohailshamas@yahoo.com>
2025-02-03 22:44:19 +0000

    Added zod validations for profile input section.

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -1,10 +1,9 @@
 import { useAuth } from "@/contexts/AuthContext";
 import { CircleMinus, CirclePlus } from "lucide-react";
-import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import * as zod from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 type LocationState = {
   foodId: string;
@@ -55,6 +54,45 @@ const Checkout: React.FC = () => {
     formState: { errors, isValid },
   } = methods;
 
+  const profileSchema = zod.object({
+    firstName: zod.string().min(2, { message: "First name is required." }),
+    lastName: zod.string().min(2, { message: "Last name is required." }),
+    phoneNumber: zod.coerce
+      .string()
+      .min(2, { message: "Phone number is required." }),
+    city: zod.string().min(2, { message: "City is required." }),
+    address: zod.string().min(2, { message: "Address is required." }),
+    postCode: zod.string().min(2, { message: "Post code is required." }),
+    country: zod.string().min(2, { message: "Country is required." }),
+  });
+
+  type profileFormFields = zod.infer<typeof profileSchema>;
+
+  const profileResolver = zodResolver(profileSchema);
+
+  const profileFormmethods = useForm<profileFormFields>({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      phoneNumber: "",
+      city: "",
+      address: "",
+      postCode: "",
+      country: "",
+    },
+    resolver: profileResolver,
+    mode: "onChange",
+  });
+
+  const {
+    register: profileRegister,
+    watch: profileWatch,
+    setValue: profileSetValue,
+    trigger: profileTrigger,
+    handleSubmit,
+    formState: { errors: profileErrors, isValid: profileIsValid },
+  } = profileFormmethods;
+
   const increment = () => {
     const currentQuantity = Number(watch("foodQuantity"));
     setValue("foodQuantity", currentQuantity + 1);
@@ -66,103 +104,142 @@ const Checkout: React.FC = () => {
     setValue("foodQuantity", currentQuantity - 1);
     trigger("foodQuantity");
   };
-
+  const onSubmit: SubmitHandler<any> = async (data) => {
+    console.log(data);
+  };
   return (
     <div className="flex my-6 max-w-[90%] mx-auto flex-col lg:flex-row">
       <div className="w-full lg:w-3/5 grow">
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="flex flex-col space-y-4 p-4">
             <h1 className="text-2xl font-bold text-gray-700">Checkout</h1>
             <div className="flex flex-col space-y-2">
-              <label htmlFor="first-name" className="hidden">
+              <label htmlFor="firstName" className="hidden">
                 First Name
               </label>
               <input
+                {...profileRegister("firstName", {
+                  onChange: () => profileTrigger("firstName"),
+                })}
                 type="text"
-                id="first-name"
-                name="first-name"
+                id="firstName"
+                name="firstName"
                 placeholder="First Name"
-                required
                 className="p-2 border-[1px] border-gray-300 rounded-md outline-none"
               />
+              {profileErrors.firstName && (
+                <p className="text-red-500 text-sm ">
+                  {profileErrors.firstName.message}
+                </p>
+              )}
             </div>
             <div className="flex flex-col space-y-2">
-              <label htmlFor="last-name" className="hidden">
+              <label htmlFor="lastName" className="hidden">
                 Last Name
               </label>
               <input
+                {...profileRegister("lastName")}
                 type="text"
-                id="last-name"
-                name="last-name"
+                id="lastName"
+                name="lastName"
                 placeholder="Last Name"
-                required
                 className="p-2 border-[1px] border-gray-300 rounded-md outline-none"
               />
+              {profileErrors.lastName && (
+                <p className="text-red-500 text-sm ">
+                  {profileErrors.lastName.message}
+                </p>
+              )}
             </div>
             <div className="flex flex-col space-y-2">
-              <label htmlFor="email" className="hidden">
+              <label htmlFor="phoneNumber" className="hidden">
                 Phone Number
               </label>
               <input
-                type="phone-number"
-                id="phone-number"
-                name="email"
+                {...profileRegister("phoneNumber")}
+                type="number"
+                id="phoneNumber"
+                name="phoneNumber"
                 placeholder="Phone Number"
-                required
                 className="p-2 border-[1px] border-gray-300 rounded-md outline-none"
               />
+              {profileErrors.phoneNumber && (
+                <p className="text-red-500 text-sm ">
+                  {profileErrors.phoneNumber.message}
+                </p>
+              )}
             </div>
             <div className="flex flex-col space-y-2">
               <label htmlFor="address" className="hidden">
                 Address
               </label>
               <input
+                {...profileRegister("address")}
                 type="text"
                 id="address"
                 name="address"
                 placeholder="Address"
-                required
                 className="p-2 border-[1px] border-gray-300 rounded-md outline-none"
               />
+              {profileErrors.address && (
+                <p className="text-red-500 text-sm ">
+                  {profileErrors.address.message}
+                </p>
+              )}
             </div>
             <div className="flex flex-col space-y-2">
-              <label htmlFor="quantity" className="hidden">
+              <label htmlFor="city" className="hidden">
                 City
               </label>
               <input
+                {...profileRegister("city")}
                 type="text"
                 id="city"
                 name="city"
                 placeholder="City"
-                required
                 className="p-2 border-[1px] border-gray-300 rounded-md outline-none"
               />
+              {profileErrors.city && (
+                <p className="text-red-500 text-sm ">
+                  {profileErrors.city.message}
+                </p>
+              )}
             </div>
             <div className="flex flex-col space-y-2">
-              <label htmlFor="price" className="hidden">
+              <label htmlFor="postCode" className="hidden">
                 Post Code
               </label>
               <input
+                {...profileRegister("postCode")}
                 type="text"
-                id="post-code"
-                name="post-code"
+                id="postCode"
+                name="postCode"
                 placeholder="Post Code"
-                required
                 className="p-2 border-[1px] border-gray-300 rounded-md outline-none"
               />
+              {profileErrors.postCode && (
+                <p className="text-red-500 text-sm ">
+                  {profileErrors.postCode.message}
+                </p>
+              )}
             </div>
             <div className="flex flex-col space-y-2">
-              <label htmlFor="price" className="hidden">
+              <label htmlFor="country" className="hidden">
                 Country
               </label>
               <input
+                {...profileRegister("country")}
                 type="text"
                 id="country"
                 name="country"
                 placeholder="country"
-                required
                 className="p-2 border-[1px] border-gray-300 rounded-md outline-none"
               />
+              {profileErrors.country && (
+                <p className="text-red-500 text-sm ">
+                  {profileErrors.country.message}
+                </p>
+              )}
             </div>
             <button
               type="submit"
