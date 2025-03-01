891ec8e336aff6612ddde99d38edc09751395e99
sohailshams <sohailshamas@yahoo.com>
2025-03-01 23:06:15 +0000

    Created incrementFoodQuantity and decrementFoodQuantity utility functions.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,27 @@
 import { format } from "date-fns";
+import { UseFormSetValue, UseFormTrigger } from "react-hook-form";
 
 export const formatDate = (date: Date | string | null | undefined) => {
   if (date === null || date === undefined) return null;
   else return format(new Date(date), "MM/dd/yyyy");
+};
+
+export const incrementFoodQuantity = (
+  currentQuantity: number,
+  setValue: UseFormSetValue<any>,
+  trigger: UseFormTrigger<any>,
+  fieldName: string
+) => {
+  setValue(fieldName, currentQuantity + 1);
+  trigger(fieldName);
+};
+
+export const decrementFoodQuantity = (
+  currentQuantity: number,
+  setValue: UseFormSetValue<any>,
+  trigger: UseFormTrigger<any>,
+  fieldName: string
+) => {
+  setValue(fieldName, currentQuantity - 1);
+  trigger(fieldName);
 };
