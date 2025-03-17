19934b01be1c9ae925a63f098d7ddee4ebeafb01
sohailshams <sohailshamas@yahoo.com>
2025-03-17 15:09:44 +0000

    Refactored Checkout component - added logic to complete the payment, display any error or redirect user to success page.

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -1,18 +1,12 @@
 import { useAuth } from "@/contexts/AuthContext";
 import { CircleMinus, CirclePlus, X } from "lucide-react";
-import { useLocation } from "react-router-dom";
 import * as zod from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { UserProfile } from "./Types/Types";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { AxiosError } from "axios";
-import {
-  addProfile,
-  createPaymentIntent,
-  fetchUserProfile,
-  updateProfile,
-} from "@/api/api";
+import { addProfile, fetchUserProfile, updateProfile } from "@/api/api";
 import Spinner from "./Spinner";
 import ProfileForm from "./ProfileForm";
 import { toast } from "sonner";
@@ -23,26 +17,27 @@ import {
   useElements,
   PaymentElement,
 } from "@stripe/react-stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
-import { useEffect, useState } from "react";
 
-type LocationState = {
-  foodId: string;
-  quantity: number;
+type CheckoutProps = {
+  itemQuantity: number;
+  setItemQuantity: (value: number) => void;
   price: number;
   name: string;
   quantityAvailable: number;
+  clientSecret: string;
 };
 
-const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
-
-const Checkout: React.FC = () => {
-  const [clientSecret, setClientSecret] = useState("");
+const Checkout: React.FC<CheckoutProps> = ({
+  itemQuantity,
+  setItemQuantity,
+  price,
+  name,
+  quantityAvailable,
+  clientSecret,
+}) => {
   const { user } = useAuth();
-  const location = useLocation();
-  const { quantity, price, name, quantityAvailable } =
-    (location.state as LocationState) || {};
+  const stripe = useStripe();
+  const elements = useElements();
 
   const {
     data: profileData,
@@ -53,36 +48,6 @@ const Checkout: React.FC = () => {
     queryKey: ["userProfile"],
     enabled: !!user?.isProfileComplete,
   });
-
-  const { mutate: createPaymentIntentMutation, data } = useMutation({
-    mutationFn: createPaymentIntent,
-    onError: (err: AxiosError) => {
-      if (err) {
-        toast.error("Failed to create payment intent.", {
-          duration: Infinity,
-          action: {
-            label: <X />,
-            onClick: () => toast.dismiss(),
-          },
-          id: "paymentIntent-fail-toast",
-        });
-      }
-    },
-
-    onSuccess: async () => {
-      toast.success("Payment successful.");
-    },
-  });
-
-  useEffect(() => {
-    createPaymentIntentMutation({
-      Amount: 40,
-    });
-  }, []);
-
-  useEffect(() => {
-    setClientSecret(data?.clientSecret ?? "");
-  }, [data]);
 
   const schema = zod.object({
     foodQuantity: zod.coerce
@@ -103,7 +68,7 @@ const Checkout: React.FC = () => {
   type formFields = zod.infer<typeof schema>;
   const resolver = zodResolver(schema);
   const methods = useForm<formFields>({
-    defaultValues: { foodQuantity: quantity },
+    defaultValues: { foodQuantity: itemQuantity },
     resolver,
     mode: "onChange",
   });
@@ -130,11 +95,11 @@ const Checkout: React.FC = () => {
         });
       }
     },
-
     onSuccess: async () => {
       toast.success("Profile updated successfully.");
     },
   });
+
   const handleProfileSubmit = async (
     data: Omit<UserProfile, "id"> & { userId: number | undefined }
   ) => {
@@ -155,81 +120,119 @@ const Checkout: React.FC = () => {
     });
   }
 
-  if (!clientSecret || !stripePromise) return null;
+  const handlePayment = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const { error: submitError } = await elements.submit();
+
+    if (submitError) {
+      toast.error(submitError.message, {
+        duration: Infinity,
+        action: {
+          label: <X />,
+          onClick: () => toast.dismiss(),
+        },
+        id: "paymentProcess-fail-toast",
+      });
+    }
+    // Remember to update the return_url in production;
+    // Also update the BE to store the order and fetch it in success page
+    const { error } = await stripe.confirmPayment({
+      elements,
+      clientSecret,
+      confirmParams: {
+        return_url: `http://localhost:5173/payment-success`,
+      },
+    });
+
+    if (error) {
+      toast.error(error.message, {
+        duration: Infinity,
+        action: {
+          label: <X />,
+          onClick: () => toast.dismiss(),
+        },
+        id: "payment-error-toast",
+      });
+    }
+  };
 
   return (
-    <Elements stripe={stripePromise} options={{ clientSecret }}>
-      <div className="flex my-6 max-w-[90%] mx-auto flex-col lg:flex-row text-gray-700">
-        <div className="w-full lg:w-3/5 grow">
-          <ProfileForm
-            profileData={profileData}
-            onSubmit={handleProfileSubmit}
-            status={status as MutationStatus}
-          />
-        </div>
-        <div className="w-full lg:w-2/5 flex-none text-gray-700">
-          <div className="p-4">
-            <h1 className="text-2xl font-bold">Order Summary</h1>
-            <div className="flex justify-between my-2">
-              <p>{name}</p>
-              <div className="flex items-center space-x-2">
-                <CircleMinus
-                  className="cursor-pointer"
-                  onClick={() =>
-                    decrementFoodQuantity(
-                      Number(watch("foodQuantity")),
-                      setValue,
-                      trigger,
-                      "foodQuantity"
-                    )
-                  }
-                />
-                <input
-                  className={`w-10 outline-none border-[1px] border-gray-300 [&::-webkit-inner-spin-button]:appearance-none text-center  ${
-                    errors.foodQuantity ? "border-red-500" : "border-gray-300"
-                  }`}
-                  type="number"
-                  {...register("foodQuantity")}
-                />
-                <CirclePlus
-                  className="cursor-pointer"
-                  onClick={() =>
-                    incrementFoodQuantity(
-                      Number(watch("foodQuantity")),
-                      setValue,
-                      trigger,
-                      "foodQuantity"
-                    )
-                  }
-                />
-              </div>
-              <p>£{price}</p>
+    <div className="flex my-6 max-w-[90%] mx-auto flex-col lg:flex-row text-gray-700">
+      <div className="w-full lg:w-3/5 grow">
+        <ProfileForm
+          profileData={profileData}
+          onSubmit={handleProfileSubmit}
+          status={status as MutationStatus}
+        />
+      </div>
+      <div className="w-full lg:w-2/5 flex-none text-gray-700">
+        <div className="p-4">
+          <h1 className="text-2xl font-bold">Order Summary</h1>
+          <div className="flex justify-between my-2">
+            <p>{name}</p>
+            <div className="flex items-center space-x-2">
+              <CircleMinus
+                className="cursor-pointer"
+                onClick={() => {
+                  decrementFoodQuantity(
+                    Number(watch("foodQuantity")),
+                    setValue,
+                    trigger,
+                    "foodQuantity"
+                  );
+                  setItemQuantity(Number(watch("foodQuantity")));
+                }}
+              />
+              <input
+                className={`w-10 outline-none border-[1px] border-gray-300 [&::-webkit-inner-spin-button]:appearance-none text-center  ${
+                  errors.foodQuantity ? "border-red-500" : "border-gray-300"
+                }`}
+                type="number"
+                {...register("foodQuantity")}
+              />
+              <CirclePlus
+                className="cursor-pointer"
+                onClick={() => {
+                  incrementFoodQuantity(
+                    Number(watch("foodQuantity")),
+                    setValue,
+                    trigger,
+                    "foodQuantity"
+                  );
+                  setItemQuantity(Number(watch("foodQuantity")));
+                }}
+              />
             </div>
-            {errors.foodQuantity && (
-              <p className="text-red-500 text-sm mt-1 text-center">
-                {errors.foodQuantity.message}
-              </p>
-            )}
-            <div className="flex justify-between my-2">
-              <p>Total</p>
-              <p>£{price * watch("foodQuantity")}</p>
-            </div>
-            <form className="mt-4">
-              {clientSecret && <PaymentElement />}
-
-              <button
-                type="submit"
-                // disabled={!stripe}
-                className="mt-4 w-full bg-blue-600 text-white py-2 rounded-md disabled:opacity-50"
-              >
-                {/* {loading ? "Processing..." : "Pay Now"} */}
-                Pay Now
-              </button>
-            </form>
+            <p>£{price}</p>
           </div>
+          {errors.foodQuantity && (
+            <p className="text-red-500 text-sm mt-1 text-center">
+              {errors.foodQuantity.message}
+            </p>
+          )}
+          <div className="flex justify-between my-2">
+            <p>Total</p>
+            <p>£{price * watch("foodQuantity")}</p>
+          </div>
+          <form onSubmit={handlePayment} className="mt-4">
+            {!stripe || !clientSecret ? <Spinner /> : <PaymentElement />}
+
+            <button
+              type="submit"
+              disabled={!isValid}
+              className="mt-4 w-full bg-blue-600 text-white py-2 rounded-md disabled:opacity-50"
+            >
+              Pay Now
+            </button>
+          </form>
         </div>
       </div>
-    </Elements>
+    </div>
   );
 };
 export default Checkout;
