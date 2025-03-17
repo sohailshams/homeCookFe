5d2958b9d1a478b74b08853e121f510f2ce7958c
sohailshams <sohailshamas@yahoo.com>
2025-03-17 15:08:06 +0000

    Created PaymentSuccess component to redirect user after successful payment.

---
+++ src/components/PaymentSuccess.tsx
@@ -0,0 +1,10 @@
+const PaymentSuccess = () => {
+  return (
+    <div>
+      <h1 className="text-center text-4xl mt-5">Payment Success</h1>
+      <p className="text-center mt-6">Thank you for your payment</p>
+    </div>
+  );
+};
+
+export default PaymentSuccess;
