6edc5c6349d6fc39e497aa3166e5dd6f34563be7
sohailshams <sohailshamas@yahoo.com>
2025-12-30 23:43:15 +0000

    Update Spinner with fullScreen prop.

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,9 +1,14 @@
-const Spinner: React.FC = () => {
+import { cn } from "@/lib/utils";
+
+type SpinnerProps = {
+  fullScreen?: boolean;
+}
+const Spinner: React.FC<SpinnerProps> = ({ fullScreen = true }) => {
   return (
-    <div className="flex items-center justify-center min-h-[300px] bg-white">
+    <div className={cn("z-50 flex items-center justify-center", fullScreen && "fixed inset-0 bg-black/30")}>
       <div className="relative flex items-center justify-center">
-        <div className="w-24 h-24 border-2 border-blue-500 border-dashed rounded-full animate-spin-slow"></div>
-        <span className="absolute text-blue-500">Loading...</span>
+        <div className="w-24 h-24 border-2 border-black border-dashed rounded-full animate-spin-slow"></div>
+        <span className="absolute text-black">Loading...</span>
       </div>
     </div>
   );
