14b7dce847a9dfd21c470def2bd66fd38ce39d8e
sohailshams <sohailshamas@yahoo.com>
2026-01-06 23:27:36 +0000

    Update id to string.

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -11,7 +11,7 @@ import {
 import { useNavigate } from "react-router-dom";
 
 export type User = {
-  id: number;
+  id: string;
   userName: string;
   userEmail: string | null;
   isProfileComplete: boolean;
